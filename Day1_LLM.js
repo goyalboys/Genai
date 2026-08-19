fa43a5823ac8f473e0f101cbf5addd3f52927d3d
vineet goyal <vineetgoyal@GGN1LPT252.local>
2026-08-19 09:42:24 +0530

    refactor

--- Day1_LLM.js
+++ Day1_LLM.js
@@ -8,21 +8,13 @@ const History = []
 
 async function Chatting(userProblem) {
 
-  History.push({
-    role:'user',
-    parts:[{text:userProblem}]
-  })
+  History.push({ role:'user', parts:[{text:userProblem}] })
 
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
     contents: History
   });
-  
-
-  History.push({
-    role:'model',
-    parts:[{text:response.text}]
-  })
+  History.push({ role:'model', parts:[{text:response.text}] })
   
   console.log("\n");
   console.log(response.text);
